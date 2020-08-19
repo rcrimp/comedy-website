3e85a04435088ac14c1aa1554e0682fe17afd539
Reuben Crimp <reubencrimp@gmail.com>
2020-08-20 01:49:18 +1200

    social media links and bio

--- scripts/populate-comedian-profiles.js
+++ scripts/populate-comedian-profiles.js
@@ -1,10 +1,76 @@
   var premium = [
-    {name: 'Jonathan Falconer', flag: 'usa.png'},
-    {name: 'Harriet Moir', flag: 'nz.png'},
-    {name: 'Reuben Crisp', flag: 'nz.png'},
-    {name: 'Simon Kingsley-Holmes', flag: 'uk.png'},
-    {name: 'Brittany Cardwell', flag: 'usa.png'},
-    {name: 'Aaron Davies', flag: 'wales.png'},
+    {
+      name: 'Jonathan Falconer',
+      flag: 'usa.png',
+      social: {
+        facebook: '',
+        instagram: '',
+        twitter: '',
+        email: '',
+        site: '', 
+      },
+      bio: ''
+    },
+    {
+      name: 'Harriet Moir',
+      flag: 'nz.png',
+      social: {
+        facebook: '',
+        instagram: '',
+        twitter: '',
+        email: '',
+        site: '', 
+      },
+      bio: ''
+    },
+    {
+      name: 'Reuben Crisp',
+      flag: 'nz.png',
+      social: {
+        facebook: 'facebook.com/phyterjet',
+        instagram: 'instagram.com/reuben_crisp',
+        twitter: 'twitter.com/reuben_crisp',
+        email: 'reubencrimp@gmail.com',
+        site: 'reubencrimp.com', 
+      },
+      bio: 'test bio.',
+    },
+    {
+      name: 'Simon Kingsley-Holmes',
+      flag: 'uk.png',
+      social: {
+        facebook: '',
+        instagram: '',
+        twitter: '',
+        email: '',
+        site: '', 
+      },
+      bio: ''
+    },
+    {
+      name: 'Brittany Cardwell',
+      flag: 'usa.png',
+      social: {
+        facebook: '',
+        instagram: '',
+        twitter: '',
+        email: '',
+        site: '', 
+      },
+      bio: ''
+    },
+    {
+      name: 'Aaron Davies',
+      flag: 'wales.png',
+      social: {
+        facebook: '',
+        instagram: '',
+        twitter: '',
+        email: '',
+        site: '', 
+      },
+      bio: ''
+    },
   ];
   var comedians = [
     'Ian Loughran',
@@ -39,7 +105,7 @@
   for (f of premium) {
     $("#comedian-list").append(
       `<div class="col s4 m3 l2">
-        <div class="card hoverable">
+        <div class="profile-card card hoverable">
           <div class="card-image modal-trigger waves-effect" href="#${f.name.replace(' ', '-')}-modal">
             <img class="" src="img/comedians/${f.name}.jpg" alt="">
           </div>
@@ -52,19 +118,17 @@
             <img class="circle responsive-img" src="img/comedians/${f.name}.jpg" alt="">
             <div class="row"/>
             <div class="row">
-              <div class="col s2 offset-s1"><a href="http://www.facebook.com/phyterjet" class="btn-floating btn-small black"><i class="fab fa-facebook"></i></a></div>
-              <div class="col s2"><a href="http://www.instagram.com/reuben_crisp" class="btn-floating btn-small black"><i class="fab fa-instagram"></i></a></div>
-              <div class="col s2"><a href="http://www.instagram.com/reuben_crisp" class="btn-floating btn-small black"><i class="fab fa-twitter"></i></a></div>
-              <div class="col s2"><a href="http://www.instagram.com/reuben_crisp" class="btn-floating btn-small black"><i class="material-icons">email</i></a></div>
-              <div class="col s2"><a href="http://www.instagram.com/reuben_crisp" class="btn-floating btn-small black"><i class="material-icons">public</i></a></div>
+              <div class="col s2 offset-s1"><a href="http://${f.social.facebook}" class="btn-floating btn-small black"><i class="fab fa-facebook"></i></a></div>
+              <div class="col s2"><a href="http://${f.social.instagram}" class="btn-floating btn-small black"><i class="fab fa-instagram"></i></a></div>
+              <div class="col s2"><a href="http://${f.social.twitter}" class="btn-floating btn-small black"><i class="fab fa-twitter"></i></a></div>
+              <div class="col s2"><a href="mailto:${f.social.email}" class="btn-floating btn-small black"><i class="material-icons">email</i></a></div>
+              <div class="col s2"><a href="http://${f.social.site}" class="btn-floating btn-small black"><i class="material-icons">public</i></a></div>
 
             </div>
           </div>
           <h5 class="col s10   m5 l6 red-text text-darken-4">${f.name}</h5>
           <img class="col s2 m1" title="NZ" src="img/${f.flag}" alt="" class="reponsive-img"/>
-          <p class="col m6 l7">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-          </h5>
+          <p class="col m6 l7">${f.bio}</h5>
         </div>
         <div class="modal-footer">
           <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
@@ -75,7 +139,7 @@
   for (f of comedians.sort()) {
     $("#comedian-list").append(
       `<div class="col s3 m2 l1">
-        <div class="profile-card card hoverable">
+        <div class="profile-card-small card hoverable">
           <div class="card-image">
             <img class="" src="img/comedians/${f}.jpg" alt="">
           </div>
